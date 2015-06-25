882e7b995c116369e022e86dcd1c56f630de295d
Viacheslav V Kovalevskyi <viacheslav@b0noi.com>
2015-06-25 20:54:32 +0100

    Field - created

---
+++ src/io/hexlet/xo/model/Field.java
@@ -0,0 +1,24 @@
+package io.hexlet.xo.model;
+
+
+import java.awt.*;
+
+public class Field {
+
+    private static final int FIELD_SIZE = 3;
+
+    private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
+
+    public int getSize() {
+        return FIELD_SIZE;
+    }
+
+    public Figure getFigure(final Point point) {
+        return field[point.x][point.y];
+    }
+
+    public void setFigure(final Point point, final Figure figure) {
+        field[point.x][point.y] = figure;
+    }
+
+}
