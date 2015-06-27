0eecf7f762ff424ba9cc655f574646f22a426f12
Viacheslav V Kovalevskyi <viacheslav@b0noi.com>
2015-06-27 20:33:47 +0100

    Field - refactoring (rules part have been removed)

--- src/io/hexlet/xo/model/Field.java
+++ src/io/hexlet/xo/model/Field.java
@@ -27,13 +27,9 @@ public class Field {
         return field[point.x][point.y];
     }
 
-    public void setFigure(final Point point, final Figure figure) throws InvalidPointException,
-                                                                        AlreadyOccupiedException {
+    public void setFigure(final Point point, final Figure figure) throws InvalidPointException {
         if (!checkPoint(point)) {
             throw new InvalidPointException();
-        }
-        if (field[point.x][point.y] != null) {
-            throw new AlreadyOccupiedException();
         }
         field[point.x][point.y] = figure;
     }
