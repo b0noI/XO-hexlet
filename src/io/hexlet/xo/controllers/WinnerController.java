913acf5be25137322aa918764d650290bf37675b
Viacheslav V Kovalevskyi <viacheslav@b0noi.com>
2015-06-27 21:08:09 +0100

    new version of the WinnerController (recursion)

---
+++ src/io/hexlet/xo/controllers/WinnerController.java
@@ -0,0 +1,60 @@
+package io.hexlet.xo.controllers;
+
+
+import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Figure;
+import io.hexlet.xo.model.exceptions.InvalidPointException;
+
+import java.awt.*;
+
+public class WinnerController {
+
+    public Figure getWinner(final Field field) {
+        try {
+            for (int i = 0; i < 3; i++)
+                if (check(field, new Point(i, 0), p -> new Point(p.x, p.y + 1)))
+                    return field.getFigure(new Point(i, 0));
+
+            for (int i = 0; i < 3; i++)
+                if (check(field, new Point(i, 0), p -> new Point(p.x + 1, p.y)))
+                    return field.getFigure(new Point(0, i));
+
+            if (check(field, new Point(0, 0), p -> new Point(p.x + 1, p.y + 1)))
+                return field.getFigure(new Point(0, 0));
+
+            if (check(field, new Point(0, 2), p -> new Point(p.x + 1, p.y - 1)))
+                return field.getFigure(new Point(1, 1));
+
+        } catch (InvalidPointException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private boolean check(final Field field,
+                          final Point currentPoint,
+                          final IPointGenerator pointGenerator) {
+        final Figure currentFigure;
+        final Figure nextFigure;
+        final Point nextPoint = pointGenerator.next(currentPoint);
+        try {
+            currentFigure = field.getFigure(currentPoint);
+            nextFigure = field.getFigure(nextPoint);
+        } catch (final InvalidPointException e) {
+            return true;
+        }
+
+        if (currentFigure == null) return false;
+
+        if (currentFigure != nextFigure) return false;
+
+        return check(field, nextPoint, pointGenerator);
+    }
+
+    private interface IPointGenerator {
+
+        Point next(final Point point);
+
+    }
+
+}
