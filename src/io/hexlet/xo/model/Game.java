5c8843d951d9bbbaaaba489b6fd2dfb0b2c66917
Viacheslav V Kovalevskyi <viacheslav@b0noi.com>
2015-06-27 20:24:52 +0100

    Game - created

---
+++ src/io/hexlet/xo/model/Game.java
@@ -0,0 +1,32 @@
+package io.hexlet.xo.model;
+
+
+public class Game {
+
+    private final Player[] players;
+
+    private final Field field;
+
+    private final String name;
+
+    public Game(final Player[] players,
+                final Field field,
+                final String name) {
+        this.players = players;
+        this.field = field;
+        this.name = name;
+    }
+
+    public Player[] getPlayers() {
+        return players;
+    }
+
+    public Field getField() {
+        return field;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
