fc59b427ea61fdb94d40a327b717a56ba43614ef
Viacheslav V Kovalevskyi <viacheslav@b0noi.com>
2015-07-05 21:51:05 +0100

    CLI v1

---
+++ src/io/hexlet/xo/XOCLI.java
@@ -0,0 +1,29 @@
+package io.hexlet.xo;
+
+
+import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Figure;
+import io.hexlet.xo.model.Game;
+import io.hexlet.xo.model.Player;
+import io.hexlet.xo.view.ConsoleView;
+
+public class XOCLI {
+
+    public static void main(final String[] args) {
+        final String name1 = "Gleb";
+        final String name2 = "Slava";
+
+        final Player[] players = new Player[2];
+        players[0] = new Player(name1, Figure.X);
+        players[1] = new Player(name2, Figure.O);
+
+        final Game gameXO = new Game(players, new Field(3), "XO");
+
+        final ConsoleView consoleView = new ConsoleView();
+        consoleView.show(gameXO);
+        while(consoleView.move(gameXO)) {
+            consoleView.show(gameXO);
+        }
+    }
+
+}
