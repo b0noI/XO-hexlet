1a89f8dd3348a2907c7fbc365345361efbd597ec
Elishanto <elishanto@gmail.com>
2015-07-10 12:37:59 +0300

    Point - created

---
+++ src/io/hexlet/xo/model/Point.java
@@ -0,0 +1,19 @@
+package io.hexlet.xo.model;
+
+public class Point {
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
