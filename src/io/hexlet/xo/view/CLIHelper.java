bd506803c2701dd3216e01d4b4b897c7a5029e91
Alex <hattkis@gmail.com>
2015-08-24 00:32:24 +0300

    CLIHelper update

--- src/io/hexlet/xo/view/CLIHelper.java
+++ src/io/hexlet/xo/view/CLIHelper.java
@@ -2,18 +2,14 @@ package io.hexlet.xo.view;
 
 public class CLIHelper {
 
-    static final int screenLinesCount = 5;
+    static final int screenLinesCount = 100;
 
-    public static void cleanScreen() {
-        try
-        {
-            for (int i = 0; i < screenLinesCount; ++i)  System.out.println("\b");
-            //Runtime.getRuntime().exec("cls");
-        }
-        catch (final Exception e)
-        {
-            e.printStackTrace();
-        }
+    public static void cleanScreenDefault() {
+
+            for (int i = 0; i < screenLinesCount; ++i)  System.out.println();
+
     }
+
 }
+
 
