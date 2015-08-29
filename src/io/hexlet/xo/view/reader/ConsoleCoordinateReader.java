25f389f7ddff25f43c108eb568f20d92b6c30f0e
Alex <hattkis@gmail.com>
2015-08-29 15:12:37 +0300

    ConsoleCoordinateReader update

--- src/io/hexlet/xo/view/reader/ConsoleCoordinateReader.java
+++ src/io/hexlet/xo/view/reader/ConsoleCoordinateReader.java
@@ -5,17 +5,6 @@ import java.util.Scanner;
 
 public class ConsoleCoordinateReader implements ICoordinateReader {
 
-    /*@Override
-    public int askCoordinate(String coordinateName) {
-        System.out.format("Please input %s:", coordinateName);
-        try (final Scanner in = new Scanner(System.in)){
-            return in.nextInt();
-        } catch (final InputMismatchException e) {
-            System.out.println("0_0 olololo!!!!!");
-            return askCoordinate(coordinateName);
-        }
-    }*/
-
     @Override
     public int askCoordinate(String coordinateName) {
         System.out.format("Please input %s:", coordinateName);
